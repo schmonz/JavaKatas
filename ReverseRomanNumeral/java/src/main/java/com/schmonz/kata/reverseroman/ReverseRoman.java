85dbf0139c438411a90a0c22defb68d02d00d6f9
Amitai Schlair <aschlair@pillartechnology.com>
2015-02-10 17:27:05 -0500

    Handle VI.

--- ReverseRomanNumeral/java/src/main/java/com/schmonz/kata/reverseroman/ReverseRoman.java
+++ ReverseRomanNumeral/java/src/main/java/com/schmonz/kata/reverseroman/ReverseRoman.java
@@ -1,9 +1,31 @@
 package com.schmonz.kata.reverseroman;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReverseRoman {
 
 	public int roman2decimal(String roman) {
-		return roman.length();
+		int decimal = 0;
+
+		char[] romanDigits = roman.toCharArray();
+		for (char thisDigit : romanDigits) {
+			decimal += digit2decimal(thisDigit);
+		}
+	
+		return decimal;
+	}
+
+	private int digit2decimal(char romanDigit) {
+		Map<Character, Integer> knownDigits = new HashMap<Character, Integer>();
+		knownDigits.put('I',  1);
+		knownDigits.put('V',  5);
+
+		if (knownDigits.containsKey(romanDigit)) {
+			return knownDigits.get(romanDigit);
+		} else {
+			return 0;
+		}
 	}
 
 }
