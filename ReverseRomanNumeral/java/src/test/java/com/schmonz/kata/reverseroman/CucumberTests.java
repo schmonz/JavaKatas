472823e87950dc0c4c89e0d9465581f7d44bf78f
Amitai Schlair <aschlair@pillartechnology.com>
2015-02-10 16:58:27 -0500

    Add failing step definitions.

--- ReverseRomanNumeral/java/src/test/java/com/schmonz/kata/reverseroman/CucumberTests.java
+++ ReverseRomanNumeral/java/src/test/java/com/schmonz/kata/reverseroman/CucumberTests.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true)
+@CucumberOptions(monochrome = true, strict = true)
 
-public class ReverseRomanTests {}
+public class CucumberTests {}
---
+++ ReverseRomanNumeral/java/src/test/java/com/schmonz/kata/reverseroman/ReverseRomanSteps.java
@@ -0,0 +1,82 @@
+package com.schmonz.kata.reverseroman;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ReverseRomanSteps {
+
+	@Given("^the Roman numeral I$")
+	public void the_Roman_numeral_I() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@When("^it is converted to decimal$")
+	public void it_is_converted_to_decimal() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Then("^its value is (\\d+)$")
+	public void its_value_is(int arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral II$")
+	public void the_Roman_numeral_II() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral VI$")
+	public void the_Roman_numeral_VI() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral IV$")
+	public void the_Roman_numeral_IV() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral IX$")
+	public void the_Roman_numeral_IX() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral XC$")
+	public void the_Roman_numeral_XC() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral XCIX$")
+	public void the_Roman_numeral_XCIX() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral MCMXC$")
+	public void the_Roman_numeral_MCMXC() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral MMVIII$")
+	public void the_Roman_numeral_MMVIII() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+	@Given("^the Roman numeral XLVII$")
+	public void the_Roman_numeral_XLVII() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
