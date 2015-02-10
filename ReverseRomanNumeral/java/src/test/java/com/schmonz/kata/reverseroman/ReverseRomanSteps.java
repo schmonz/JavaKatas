7438118724db706f750dde6a838a4121d9fbba1e
Amitai Schlair <aschlair@pillartechnology.com>
2015-02-10 17:28:35 -0500

    Convert to camelCase.

--- ReverseRomanNumeral/java/src/test/java/com/schmonz/kata/reverseroman/ReverseRomanSteps.java
+++ ReverseRomanNumeral/java/src/test/java/com/schmonz/kata/reverseroman/ReverseRomanSteps.java
@@ -7,21 +7,21 @@ import static org.junit.Assert.assertEquals;
 
 public class ReverseRomanSteps {
 	String roman;
-	int actual_decimal;
+	int actualDecimal;
 
 	@Given("^the Roman numeral (.+)$")
-	public void the_roman_numeral(String roman_numeral) throws Throwable {
-		roman = roman_numeral;
+	public void theRomanNumeral(String romanNumeral) throws Throwable {
+		roman = romanNumeral;
 	}
 	
 	@When("^it is converted to decimal$")
-	public void it_is_converted_to_decimal() throws Throwable {
-		actual_decimal = new ReverseRoman().roman2decimal(roman);
+	public void itIsConvertedToDecimal() throws Throwable {
+		actualDecimal = new ReverseRoman().roman2decimal(roman);
 	}
 	
 	@Then("^its value is (\\d+)$")
-	public void its_value_is(int expected_decimal) throws Throwable {
-	    assertEquals(expected_decimal, actual_decimal);
+	public void itsValueIs(int expectedDecimal) throws Throwable {
+	    assertEquals(expectedDecimal, actualDecimal);
 	}
 	
 }
