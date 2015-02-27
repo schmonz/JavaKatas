643b565ad9eda255088aa4f915844d420341eed9
Amitai Schlair <aschlair@pillartechnology.com>
2015-02-27 13:08:22 -0500

    Add Uncle Bob's bowling game, where we left off.

---
+++ BowlingGame/test/bowlingSolution/BowlingGameTest.java
@@ -0,0 +1,57 @@
+//****************************************************************
+//* Copyright (c) 2015 Ford Motor Company. All Rights Reserved.
+//****************************************************************
+package bowlingSolution;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BowlingGameTest {
+
+    private Game g;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        g = new Game();
+    }
+
+    private void rollMany(final int n, final int pins) {
+        for (int i = 0; i < n; i++) {
+            g.roll(pins);
+        }
+    }
+
+    @Test
+    public void testGutterGame() {
+        rollMany(20, 0);
+        assertEquals(0, g.score());
+    }
+
+    @Test
+    public void testAllOnes() {
+        rollMany(20, 1);
+        assertEquals(20, g.score());
+
+    }
+
+    @Test
+    public void testOneSpare() throws Exception {
+        rollSpare();
+        g.roll(3);
+        rollMany(17, 0);
+        assertEquals(16, g.score());
+    }
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5); // spare
+    }
+
+}
